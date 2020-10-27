9b7be56e7e9c4e9aa00eb1fe1b2cb61703e188d9
Colin <colint.cron@gmail.com>
2020-10-26 22:54:28 -0400

    added tests

--- blog/tests.py
+++ blog/tests.py
@@ -20,6 +20,9 @@ class BlogTests(TestCase):
     def test_string_representation(self):
         post = Post(title='A sample title')
         self.assertEqual(str(post), post.title)
+    
+    def test_get_absolute_url(self):
+        self.assertEqual(self.post.get_absolute_url(), '/post/1/')
 
     def test_post_content(self):
         self.assertEqual(f'{self.post.title}', 'a good title')
@@ -40,4 +43,24 @@ class BlogTests(TestCase):
         self.assertContains(response, 'a good title')
         self.assertTemplateUsed(response, 'post_detail.html')
 
-# Create your tests here.
+    def test_post_create_view(self):
+        response = self.client.post(reverse('post_new'), {
+            'title': 'New Title',
+            'body': 'New Text',
+            'author': self.user.id,
+        })
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(Post.objects.last().title, 'New Title')
+        self.assertEqual(Post.objects.last().body, 'New Text')
+
+    def test_post_update_view(self):
+        response = self.client.post(reverse('post_edit', args='1'), {
+            'title': 'Updated title',
+            'body': 'Updated Text'
+        })
+        self.assertEqual(response.status_code, 302)
+    
+    def test_post_delete_view(self):
+        response = self.client.post(
+            reverse('post_delete', args='1'))
+        self.assertEqual(response.status_code, 302)
